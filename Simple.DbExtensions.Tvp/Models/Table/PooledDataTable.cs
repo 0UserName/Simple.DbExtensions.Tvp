44b171bcafffc8f5275012f1f1a114f3292d6621
0username <net.fedorax64@gmail.com>
2025-02-13 02:06:23 +0300

    PooledDataTable added

---
+++ Simple.DbExtensions.Tvp/Models/Table/PooledDataTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.ObjectPool;
+
+using Simple.DbExtensions.Tvp.Models.Contracts;
+
+using System.Collections.Generic;
+using System.Data;
+
+namespace Simple.DbExtensions.Tvp.Models.Table
+{
+    public sealed class PooledDataTable : DataTable, IResettable
+    {
+        private void InitColumns(IColumnMetadata[] metadata)
+        {
+            for (int i = 0; i < metadata.Length; i++)
+            {
+                Columns.Add(metadata[i].CreateColumn());
+            }
+        }
+
+        /// <summary>
+        /// Finds and updates
+        /// a specific row. If no matching row is 
+        /// found, a new row is created using the 
+        /// given values.
+        /// </summary>
+        public void LoadDataRow<TRow>(IEnumerable<TRow> rows) where TRow : class, ITableValued
+        {
+            BeginLoadData();
+
+            foreach (TRow row in rows)
+            {
+                LoadDataRow(row.GetRow(), LoadOption.PreserveChanges);
+            }
+
+            EndLoadData();
+        }
+
+        /// <inheritdoc/>
+        public bool TryReset()
+        {
+            Clear();
+
+            return true;
+        }
+
+        internal PooledDataTable(ITableMetadata table, IColumnMetadata[] columns) : base(table.Name)
+        {
+            BeginInit();
+
+            CaseSensitive = table.CaseSensitive;
+
+            InitColumns(columns);
+
+            EndInit();
+        }
+    }
+}
